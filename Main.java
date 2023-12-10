4d6e1689ab7d8a1b0efa6fbacb10b5a851afd534
master <time_and_work@mail.ru>
2023-12-10 21:59:03 +0300

    Works

--- Main.java
+++ Main.java
@@ -51,8 +51,8 @@ public class Main {
                 throw new ScannerExeption("Выражение не правильное!");
             } else{
                 sum = Integer.parseInt(otvet(num1,num2,progress));
-                String[] rims = new String[] {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"};
-                a=rims[sum-1];
+//                String[] rims = new String[] {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"};
+                a = convert(sum);
             }
         }
         else {
@@ -60,6 +60,64 @@ public class Main {
             a = Integer.toString(sum);
         }
         return a;
+    }
+    public static String romanDigit(int n, String one, String five, String ten){
+
+        if(n >= 1)
+        {
+            if(n == 1)
+            {
+                return one;
+            }
+            else if (n == 2)
+            {
+                return one + one;
+            }
+            else if (n == 3)
+            {
+                return one + one + one;
+            }
+            else if (n==4)
+            {
+                return one + five;
+            }
+            else if (n == 5)
+            {
+                return five;
+            }
+            else if (n == 6)
+            {
+                return five + one;
+            }
+            else if (n == 7)
+            {
+                return five + one + one;
+            }
+            else if (n == 8)
+            {
+                return five + one + one + one;
+            }
+            else if (n == 9)
+            {
+                return one + ten;
+            }
+
+        }
+        return "";
+    }
+    public static String convert(int number){
+
+        String romanOnes = romanDigit( number%10, "I", "V", "X");
+        number /=10;
+        String romanTens = romanDigit( number%10, "X", "L", "C");
+        number /=10;
+        String romanHundreds = romanDigit(number%10, "C", "D", "M");
+        number /=10;
+        String romanThousands = romanDigit(number%10, "M", "", "");
+
+        String result = romanThousands + romanHundreds + romanTens + romanOnes;
+        return result;
+
     }
     public static String otvet(String num1, String num2, String progress) throws ScannerExeption{
         int number1 = Integer.parseInt(num1);
